65854a051f12dead95430a2e0503866aca63ba04
MaricruzAM <tekiwikak@gmail.com>
2026-05-03 10:20:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
 import os
 import telebot
 import google.generativeai as genai
+
 with open("ley_limpia.txt.txt", "r", encoding="utf-8") as f:
     texto_ley = f.read()
+
 MODELO_GEMINI = "gemini-2.5-flash"
 
 TOKEN = os.getenv("TOKEN_TELEGRAM")
 CLAVE_GEMINI = os.getenv("CLAVE_GEMINI")
 
 bot = telebot.TeleBot(TOKEN)
+
 genai.configure(api_key=CLAVE_GEMINI)
 client = genai.GenerativeModel(MODELO_GEMINI)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, "Hola, soy tu asistente de la Ley de Educación de NL. Pregúntame lo que quieras.")
+    bot.reply_to(
+        message,
+        "Hola, soy tu asistente de la Ley de Educación de Nuevo León. Pregúntame lo que quieras."
+    )
 
 @bot.message_handler(func=lambda message: True)
 def responder(message):
     try:
         pregunta = message.text
 
-prompt = f"""
+        prompt = f"""
 Eres un asistente experto en la Ley de Educación de Nuevo León 2026.
 
 Responde SIEMPRE en español.
 
-Explica de forma clara, pero fundamenta tus respuestas mencionando artículos cuando sea posible.
+Explica de forma clara y fundamenta tus respuestas mencionando artículos cuando sea posible.
 
 Texto de la ley:
-{texto_ley[:120000]}
+{texto_ley[:30000]}
 
 Pregunta del usuario:
 {pregunta}
 """
 
-respuesta = client.generate_content(prompt)
+        respuesta = client.generate_content(prompt)
 
         texto = respuesta.text if respuesta.text else "No pude responder."
         bot.reply_to(message, texto[:4000])
@@ -45,5 +51,3 @@ respuesta = client.generate_content(prompt)
 
 print("Bot corriendo...")
 bot.infinity_polling()
-
-
